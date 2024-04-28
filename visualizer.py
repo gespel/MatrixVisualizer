6f380f35dd068ab9396eef5cf21cd709e7822c1b
gespel <sten.heimbrodt@hotmail.de>
2024-04-28 22:44:54 +0200

    asd

--- visualizer.py
+++ visualizer.py
@@ -1,55 +1,65 @@
+import os
+import shutil
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
 import glob
 import contextlib
+import logging
 from PIL import Image
 
+logging.basicConfig(format='%(asctime)s:%(levelname)s: %(message)s', level=logging.INFO)
 
-file1 = open('BTCS_48_48_5000-1.csv', 'r')
-Lines = file1.read()
-Matrices = Lines.split("\n\n")
+class MatrixVisualizer:
+    def __init__(self):
+        pass
+
+    def visualize(self, fileName):
+        file1 = open(fileName, 'r')
+        Lines = file1.read()
+        Matrices = Lines.split("\n\n")
+        os.mkdir("tmp")
+        logging.info('Parsing File...')
+        out = []
+        for matrix in Matrices:
+            m = []
+            matrixLines = matrix.split("\n")
+            
+            for matrixLine in matrixLines:
+                tmp = []
+                splitline = matrixLine.strip().split(" ")
+                
+                for n in splitline:
+                    if n:
+                        tmp.append(float(n))
+                if len(tmp) > 1:
+                    m.append(tmp)
+            out.append(m)
+
+        logging.info('Rendering frame images...')
+        for x in range(0, len(out), 20):
+            plt.imshow(out[x], cmap='copper', interpolation='nearest')
+            plt.colorbar()
+
+            plt.savefig(f'tmp/{x}.png', dpi=300)
+            plt.close()
 
 
-out = []
-for matrix in Matrices:
-    m = []
-    matrixLines = matrix.split("\n")
-    
-    for matrixLine in matrixLines:
-        tmp = []
-        splitline = matrixLine.strip().split(" ")
-        
-        for n in splitline:
-            if n:
-                tmp.append(float(n))
-        if len(tmp) > 1:
-            m.append(tmp)
-    out.append(m)
+        logging.info('Rendering GIF...')
+        fp_in = "tmp/*.png"
+        fp_out = f"{fileName.split('.')[0]}.gif"
 
+        image_paths = sorted(glob.glob(fp_in), key=lambda x: int(x.split("/")[1].split('.')[0]))
 
-for x in range(0, len(out), 20):
-    plt.imshow(out[x], cmap='copper', interpolation='nearest')
-    plt.colorbar()
+        images = []
 
-    plt.savefig(f'{x}.png', dpi=300)
-    plt.close()
+        for path in image_paths:
+            images.append(Image.open(path))
 
+        images[0].save(fp=fp_out, format='GIF', append_images=images[1:],
+                    save_all=True, duration=20, loop=0)
+        logging.info('Cleaning up...')
+        shutil.rmtree("tmp")
 
-
-fp_in = "*.png"
-fp_out = "matrix.gif"
-
-# Liste der Dateipfade zu den Bildern sortieren
-image_paths = sorted(glob.glob(fp_in), key=lambda x: int(x.split('.')[0]))
-
-# Liste zum Speichern der geladenen Bilder
-images = []
-
-# Bilder laden
-for path in image_paths:
-    images.append(Image.open(path))
-
-# GIF erstellen
-images[0].save(fp=fp_out, format='GIF', append_images=images[1:],
-               save_all=True, duration=20, loop=0)
+mv = MatrixVisualizer()
+mv.visualize("BTCS_48_48_5000.csv")
